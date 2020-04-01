18e123a8cbbc6b9df13732ec3c9b4967034ee268
EmilioPadilla <padillam_@hotmail.com>
2020-03-31 18:39:58 -0600

    terminar lab14

--- Lab14/index.php
+++ Lab14/index.php
@@ -1,4 +1,30 @@
 <?php
+  require_once("model.php");
   include("html/_header.html");
 
+  include("html/_table.html");
+
+  if (isset($_POST["Materiales"])) {
+      $clave = htmlspecialchars($_POST["Materiales"]);
+  } else {
+      $clave = "";
+  }
+
+  if (isset($_POST["Proveedores"])) {
+      $rfc = htmlspecialchars($_POST["Proveedores"]);
+  } else {
+      $rfc = "";
+  }
+
+  if (isset($_POST["Proyectos"])) {
+      $numero = htmlspecialchars($_POST["Proyectos"]);
+  } else {
+      $numero = "";
+  }
+
+  echo consultar_existencia($clave, $rfc, $numero);
+
+
+
+  include("html/_footer.html")
  ?>
