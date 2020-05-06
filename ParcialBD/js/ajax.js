de0490456528db968c0ef2bb8b447b4d6db2d392
EmilioPadilla <padillam_@hotmail.com>
2020-05-06 12:34:03 -0500

    Agregar registrar incidente con JQuery

--- ParcialBD/js/ajax.js
+++ ParcialBD/js/ajax.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
 
 //Funcion para registrar nuevo incidente
 function registrar() {
-  console.log("Esta fucnionando");
     //$.post manda la petición asíncrona por el método post. También existe $.get
-    $.post("controller_registrar.php", {
+    $.get("controller_registrar.php", {
         lugar_id: $("#lugar").val(),
         tipo_id: $("#tipo").val()
     }).done(function (data) {
